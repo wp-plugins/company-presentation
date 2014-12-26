2ca3e2e0a818b55ffd7635bbbd924a701a6274c4
Ralph Rezende <Ralph Rezende@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-26 22:36:55 +0000

    New version 2.0.5
    git-svn-id: https://plugins.svn.wordpress.org/company-presentation/tags/2.0.2@1054741 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ php/careerpagespluginhandler.php
@@ -0,0 +1,41 @@
+<?php
+error_reporting(E_ALL);
+header("content-type: text/html; Charset: utf-8");
+setlocale(LC_ALL, "en_GB");
+
+$action = $_POST['action']; 
+
+if ($action == "getTeamsHtml") {
+	echo json_encode(careerpages($action, "teams", $_POST['template'], $_POST['key'], $_POST['teams'], $_POST['subdir'], $_POST['breadcrumbs']));
+
+} elseif ($action == "getTeamHtml") {
+	echo json_encode(careerpages($action, "team", $_POST['template'], $_POST['key'], $_POST['team'], $_POST['subdir'], $_POST['breadcrumbs']));
+
+} elseif ($action == "getProfileHtml") {
+	echo json_encode(careerpages($action, "profile", $_POST['template'], $_POST['key'], $_POST['profile'], $_POST['subdir'], $_POST['breadcrumbs']));
+}
+
+function careerpages($action, $level, $template, $key, $ids, $subdir, $breadcrumbs) {
+	//set POST variables
+	$fields = array(
+		'action' => urlencode($action),
+		'key' => urlencode($key),
+		$level => urlencode($ids),
+		'template' => urlencode($template),
+		'subdir' => urlencode($subdir),
+		'breadcrumbs' => $breadcrumbs
+	);
+	
+	// create curl resource 
+	$ch = curl_init(); 
+	curl_setopt($ch, CURLOPT_URL, "https://".($subdir ? $subdir."." : "")."prodii.com/common/careerpages/careerpageshandler.php"); 
+	curl_setopt($ch, CURLOPT_POST, count($fields));
+	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($fields));
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+	$output = json_decode(curl_exec($ch)); 
+	curl_close($ch);
+
+	return $output;     
+}
+?>
